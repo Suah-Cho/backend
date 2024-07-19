7b840969017abbf9d87aa068a0321dc9cc40abc2
SuahCho29 <suajo@croft-ai.com>
2024-07-19 11:31:54 +0900

    change url

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import requests
+import os
+
 app = FastAPI()
 
 @app.get('/')
 def get_test():
     return JSONResponse(status_code=200, content={"message": "Hello, World!"})
 
+slack_url = os.environ.get('TEST_SLAK_URL')
 
 @app.post('/')
 def event_grid(data: dict):
     print(data)
-    response = requests.post('https://hooks.slack.com/services/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', json={"text": str(data)})
+    response = requests.post(url=slack_url, json={"text": str(data)})
     print("response", response)
     return JSONResponse(status_code=200, content={"message": "response"})

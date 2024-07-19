149ffc8b41b26bd933a1a3d439907a79da3bac61
SuahCho29 <suajo@croft-ai.com>
2024-07-19 11:29:14 +0900

    add event grid post

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-
+import requests
 app = FastAPI()
 
 @app.get('/')
 def get_test():
     return JSONResponse(status_code=200, content={"message": "Hello, World!"})
-    
+
+
+@app.post('/')
+def event_grid(data: dict):
+    print(data)
+    response = requests.post('https://hooks.slack.com/services/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', json={"text": str(data)})
+    print("response", response)
+    return JSONResponse(status_code=200, content={"message": "response"})

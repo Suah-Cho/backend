ee33ff8b786f36d4b1c6aeb17126dc604852ac65
SuahCho29 <suajo@croft-ai.com>
2024-07-31 09:03:24 +0900

    test env

--- app.py
+++ app.py
@@ -4,17 +4,16 @@ import requests
 import os
 
 app = FastAPI()
+API_MODE = os.getenv('API_MODE')
 
 @app.get('/')
 def get_test():
     return JSONResponse(status_code=200, content={"message": "Hello, World!"})
 
-slack_url = os.environ.get('TEST_SLAK_URL')
-print(slack_url)
 
 @app.post('/')
 def event_grid(data: dict):
     print(data)
-    response = requests.post(url=slack_url, json={"text": str(data)})
+    response = requests.post('https://hooks.slack.com/services/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', json={"text": str(data)})
     print("response", response)
     return JSONResponse(status_code=200, content={"message": "response"})
